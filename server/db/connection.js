7b85cecfabc269fd131b71ee45881e308ce0e0bf
Max Hsieh <maxhsieh0@gmail.com>
2024-08-15 16:16:04 -0700

    sup

---
+++ server/db/connection.js
@@ -0,0 +1,30 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const uri = "mongodb+srv://maxhsieh0:cI1aOzejcuW7m6bi@cluster0.gbm58yg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+try {
+  await client.connect();
+  // Send a ping to confirm a successful connection
+  await client.db("admin").command({ ping: 1 });
+  console.log(
+   "Pinged your deployment. You successfully connected to MongoDB!"
+  );
+} catch(err) {
+  console.error(err);
+}
+
+let db = client.db("employees");
+
+export default db;
